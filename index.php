2f7648b26b4329be4e37e0e293afdca7713af404
enortiz1 <55902065+enortiz1@users.noreply.github.com>
2021-03-24 21:01:13 -0300

    Cambia archivo index a php

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Resume</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <div class="container">
+    <div id="resume-header" class="row">
+      <div class="col-3">
+        <img src="https://ui-avatars.com/api/?name=Eric+Ortiz&size=255" alt="" id="profile-picture">
+      </div>
+      <div class="col">
+        <h1><?php echo 'Eric Ortiz';?></h1>
+        <h2>PHP Developer</h2>
+        <ul>
+          <li>Mail: ortiz.ericnicolas@gmail.com</li>
+          <li>Phone: +541126492506</li>
+          <li>Linkedin: https://linkedin.com/eric_nortiz</li>
+          <li>Twitter: @eric_nortiz</li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2 class="border-bottom-gray">Career Summary</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad adipisci
+        </p>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div>
+            <h3 class="border-bottom-gray">Work Experience</h3>
+            <ul>
+              <li class="work-position">
+                <h5>PHP Developer</h5>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+                <strong>Achievements:</strong>
+                <ul>
+                  <li>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                </ul>
+              </li>
+              <li class="work-position">
+                <h5>PHP Developer</h5>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+                <strong>Achievements:</strong>
+                <ul>
+                  <li>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+                  <li>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  </li>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="border-bottom-gray">Projects</h3>
+            <div class="project">
+              <h5>Project X</h5>
+              <div class="row">
+                <div class="col-3">
+                <img id="profile-picture" src="https://ui-avatars.com/api/?name=Eric+Ortiz&size=255" alt="">
+                </div>
+                <div class="col">
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto impedit quae culpa sed deserunt</p>
+                  <strong>Technologies used:</strong>
+                  <span class="badge badge-secondary">PHP</span>
+                  <span class="badge badge-secondary">HTML</span>
+                  <span class="badge badge-secondary">CSS</span>
+                </div>
+              </div>
+            </div>
+            <div class="project">
+              <h5>Project X</h5>
+              <div class="row">
+                <div class="col-3">
+                  <img src="https://ui-avatars.com/api/?name=Eric+Ortiz&size=255" alt="" id="profile-picture">
+                </div>
+                <div class="col">
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil illo quaerat blanditiis ab quod vel. Sit sint minus aut, suscipit enim possimus dicta mollitia recusandae quisquam atque nisi et excepturi?</p>
+                  <strong>Technologies used:</strong>
+                  <span class="badge badge-secondary">PHP</span>
+                  <span class="badge badge-secondary">HTML</span>
+                  <span class="badge badge-secondary">CSS</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <h3 class="border-bottom-gray">Skills & Tools</h3>
+          <h4>Backend</h4>
+          <ul>
+            <li>PHP</li>
+          </ul>
+          <h4>Frontend</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+
+          </ul>
+          <h4>Frameworks</h4>
+          <ul>
+            <li>Laravel</li>
+            <li>Bootstrap</li>
+          </ul>
+          <h3 class="border-bottom-gray">Languages</h3>
+          <ul>
+            <li>Spanish</li>
+            <li>English</li>
+          </ul>
+        </div>
+      </div>
+      <div id="resume-footer" class="row">
+        <div class="col">
+          Designed by @eric_nortiz
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
