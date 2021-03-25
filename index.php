96a60d87330e1166f7cdd6f3c8abbba3c20d91eb
enortiz1 <55902065+enortiz1@users.noreply.github.com>
2021-03-24 21:40:42 -0300

    Modifica texto en career

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <link rel="stylesheet" href="style.css" />
 </head>
 
@@ -20,7 +19,7 @@
         <img src="https://ui-avatars.com/api/?name=Eric+Ortiz&size=255" alt="" id="profile-picture">
       </div>
       <div class="col">
-        <h1><?php echo 'Eric Ortiz';?></h1>
+        <h1><?php echo 'Eric Ortiz'; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: ortiz.ericnicolas@gmail.com</li>
@@ -34,7 +33,16 @@
       <div class="col">
         <h2 class="border-bottom-gray">Career Summary</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad adipisci
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo natus doloremque quia eum perferendis maiores consequatur cum deserunt. Asperiores autem atque laudantium, modi tenetur suscipit eius ipsam porro magnam eum.
+         Veritatis eligendi quos aliquam ex fuga excepturi nulla sapiente praesentium aperiam, aliquid quis dolorem, rerum eos animi, repellendus nemo vero illo dolorum molestias quas ea accusantium quae repudiandae facere. Sunt.
+         Quia ducimus eius omnis doloremque nobis autem dignissimos nostrum neque, ea cupiditate quos harum natus, fuga dolor, magni sed voluptate debitis inventore nisi numquam velit? Ipsum nobis autem inventore sed!
+         Culpa, nulla nostrum consectetur iure error quo neque! Deleniti voluptates, optio quam fugiat non quis, at repellat quasi voluptatum, totam repudiandae voluptate neque id doloribus expedita fuga pariatur? Amet, perspiciatis?
+         Quaerat accusamus architecto a aliquam, eum fuga nulla ipsam voluptate atque eius exercitationem neque eveniet optio debitis dicta soluta molestias explicabo? Repellendus, minus quasi. Quaerat, tempore! Laboriosam culpa tempore animi.
+         Veniam in consequuntur, asperiores hic dolore, aperiam itaque sit, ad inventore impedit vero! Mollitia quis voluptatibus facilis nobis accusamus quam quisquam adipisci, quia nam, nesciunt reprehenderit iure commodi, accusantium quod?
+         Rem, vel temporibus eos nam, tempore ad cupiditate nemo vitae aliquid, recusandae modi! Possimus quisquam illo rem animi quibusdam atque praesentium iste aspernatur earum, tempore hic nobis velit sed doloribus!
+         Dignissimos in enim consequatur unde, minima vitae ipsum ad nam magni? Tenetur atque ducimus distinctio doloremque dolorem, voluptate provident dignissimos quibusdam, in quasi amet eius delectus repellat pariatur asperiores nobis.
+         Nemo laborum tenetur, unde temporibus delectus omnis illo provident? Error modi iste vel consequuntur laboriosam distinctio atque architecto aliquam perferendis fuga. Iure, nulla? Ab nobis nihil excepturi perferendis quibusdam deleniti.
+         Maiores eaque reprehenderit temporibus nulla architecto culpa consectetur laboriosam dolores recusandae molestiae aspernatur est impedit suscipit excepturi, quasi magni in necessitatibus ducimus quas eos doloremque rem deleniti! Voluptatibus, vero error.
         </p>
       </div>
       <div class="row">
@@ -85,7 +93,7 @@
               <h5>Project X</h5>
               <div class="row">
                 <div class="col-3">
-                <img id="profile-picture" src="https://ui-avatars.com/api/?name=Eric+Ortiz&size=255" alt="">
+                  <img id="profile-picture" src="https://ui-avatars.com/api/?name=Eric+Ortiz&size=255" alt="">
                 </div>
                 <div class="col">
                   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto impedit quae culpa sed deserunt</p>
@@ -147,15 +155,9 @@
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
